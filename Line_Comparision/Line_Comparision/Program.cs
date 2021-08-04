deda0ddf22c8f864d022aa23a019533a63cc8c22
DayanandaChittoor <seenu.daya5@gmail.com>
2021-08-04 19:27:04 +0530

    Added Line Comparision program

---
+++ Line_Comparision/Line_Comparision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_Comparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch!");
+        }
+    }
+}
